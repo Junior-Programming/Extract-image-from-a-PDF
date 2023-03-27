18f25fc7067f2177264133a34968d40344e3124f
Sopyan <whitemustang672@gmail.com>
2023-03-28 00:50:45 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfReader
+
+reader = PdfReader(
+    "D:/Materi pembelajaran individu/Programing/Project Python/6. Extract Images from a PDF/test.pdf")
+
+page = reader.pages[0]
+count = 0
+
+for image_file_object in page.images:
+    with open(str(count) + image_file_object.name, "wb") as fp:
+        fp.write(image_file_object.data)
+        count += 1
